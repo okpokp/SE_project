fb07e9193d059aa2aad58d02317125d1c4303fdf
Gillvy <grilltora@gmail.com>
2019-08-29 12:51:14 +0700

    teacher

--- application/views/bin/ui_tabtch.php
+++ application/views/bin/ui_tabtch.php
@@ -1,53 +1,89 @@
 <!DOCTYPE html>
 
-<div>
-
-    <a class="well btn container-fluid " style="color: black" href="<?= base_url('Controller/index') ?>">
-        root_login
-    </a>
-
-
-    <a class="well btn container-fluid" style="color: black" href="<?= base_url('Controller/home_std') ?>">
-        root_home_student
-    </a>
-
-    <!-- 0 -->
-
-    <a class="well btn container-fluid" style="color: black" href="<?= base_url('Controller/home_tch') ?>">
-        หน้าหลัก
-    </a>
-    <!-- <img src="https://cdn2.iconfinder.com/data/icons/picons-basic-2/57/basic2-158_home_house-512.png" class="img-circle" height="65" width="65" alt="Home"> -->
-
-    <!-- 1 -->
-
-    <a class="well btn container-fluid" style="color: black" href="<?= base_url('Controller/consult') ?>">
-        รายชื่อโครงงานที่เป็นที่ปรึกษา
-    </a>
-
-    <!-- 2 -->
-
-    <a class="well btn container-fluid " style="color: black" href="<?= base_url('Controller/commit') ?>">
-        รายชื่อโครงงานที่เป็นกรรมการ
-    </a>
-
-    <!-- 3 -->
-
-    <a class="well btn container-fluid" style="color: black" href="<?= base_url('Controller/db_user') ?>">
-        root_db_user
-    </a>
-
-    <!-- 4 -->
-
-    <a class="well btn container-fluid" style="color: black" href="<?= base_url('Controller/regist_tch') ?>">
-        root_register_teacher
-    </a>
-
-    <!-- 5 -->
-
-    <p>
-        <a class="well btn container-fluid" href="<?= base_url('Controller/create_group') ?>">
-            root_create_group
+<!-- <p>
+        <a class="well btn container-fluid colora" href="<?= base_url('Controller/index') ?>">
+            root_login
+            <br>
+            <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
         </a>
-    </p>
+    </p> -->
 
-</div>
+<!-- <p>
+        <a class="well btn container-fluid colora" href="<?= base_url('Controller/home_std') ?>">
+            root_home_student
+            <br>
+            <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+        </a>
+    </p> -->
+
+<p>
+    <a class="well btn container-fluid colora" href="<?= base_url('Controller/home_tch') ?>">
+        หน้าหลัก
+        <br>
+        <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+    </a>
+</p>
+
+<p>
+    <a class="well btn container-fluid colora" href="<?= base_url('Controller/consult') ?>">
+        รายชื่อโครงงานที่เป็นที่ปรึกษา
+        <br>
+        <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+    </a>
+</p>
+
+<p>
+    <a class="well btn container-fluid colora" href="<?= base_url('Controller/commit') ?>">
+        รายชื่อโครงงานที่เป็นกรรมการ
+        <br>
+        <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+    </a>
+</p>
+
+<p>
+    <a class="well btn container-fluid colora" href="<?= base_url('Controller/log_score') ?>">
+        คะแนนความสม่ำเสมอ
+        <br>
+        <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+    </a>
+</p>
+
+<p>
+    <a class="well btn container-fluid colora" href="<?= base_url('Controller/test_room') ?>">
+        ห้องสอบ
+        <br>
+        <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+    </a>
+</p>
+
+<p>
+    <a class="well btn container-fluid colora" href="<?= base_url('Controller/test_score') ?>">
+        ลงคะแนน
+        <br>
+        <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+    </a>
+</p>
+
+<!-- <p>
+        <a class="well btn container-fluid colora" href="<?= base_url('Controller/db_user') ?>">
+            root_db_user
+            <br>
+            <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+        </a>
+    </p> -->
+
+<!-- <p>
+        <a class="well btn container-fluid colora" href="<?= base_url('Controller/regist_tch') ?>">
+            root_register_teacher
+            <br>
+            <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+        </a>
+    </p> -->
+
+<!-- <p>
+        <a class="well btn container-fluid colora" href="<?= base_url('Controller/create_group') ?>">
+            root_create_group
+            <br>
+            <img src="<?= base_url('./image/img_home.png') ?>" class="img-circle" height="65" width="65" alt="Avatar">
+        </a>
+    </p> -->
