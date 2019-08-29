ecf3757bcc15550b584ed73395fcf3df6a0d025e
chakkapong <54110632+au2199@users.noreply.github.com>
2019-08-29 22:48:27 +0700

    r

---
+++ application/views/teacher/showconsult.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang='en' dir='ltr'>
+
+<!--############################################## Head ###########################################################################-->
+<head>
+	<!-- Okp config -->
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Load style.css file, which store in css folder -->
+
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css'>
+	<!-- jQuery library -->
+	<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
+	<!-- Latest compiled JavaScript -->
+	<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js'></script>
+	<script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js'></script>
+    <link rel="stylesheet" href="<?php echo base_url() ?>css/style.css">
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<meta charset='utf-8'>
+	<script>
+		$(function() {
+			$('#ui_main').load('<?= base_url('Controller/ui_main') ?>');
+			$('#ui_footer').load('<?= base_url('Controller/ui_footer') ?>');
+			$('#ui_tab').load('<?= base_url('Controller/ui_tabstd') ?>');
+		})
+	</script>
+    <style media="screen">
+        .bgimg {
+            background-image: url('<?= base_url('./image/back_inweb.jpg') ?>');
+            min-height: 100%;
+            background-position: center;
+            background-size: cover;
+            /* z-index: -1; */
+        }
+    </style>
+	<title>Index</title>
+</head>
+<!--############################################## Header ###########################################################################-->
+<header>
+	<div id='ui_main'></div>
+</header>
+<!--############################################## Body ###########################################################################-->
+<body>
+	<div class="container-fluid text-center">
+		<div class="row">
+			<!-- Bar -->
+			<div class="col-sm-2 well colora" style=" margin-top: 10px;">
+				<div id='ui_tab'></div>
+			</div>
+			<!-- End Bar -->
+			<div class="col-sm-10 well text-left size bgimg">
+        <form class="needs-validation" novalidate>
+
+              <div class="form-row">
+                <div class="col-sm-6">
+
+                	<!--g1-->
+                 	<div>
+                 		<table class="table table-bordered positionin">
+                 			<tbody>
+                 				<tr class="success">
+							        <td class="h_table" style="background-color: #7C0000">ที่ปรึกษา</td>
+							        <td >
+							        	<table>
+								          <tr>
+								            <td>Peter</td>
+								          </tr>
+								          <tr>
+								            <td>Peter</td>
+								          </tr>
+
+								        </table>
+							        </td>
+							      
+						        </tr>
+                 			</tbody>
+                 		</table>
+                 	</div>
+
+                 	<!--g2-->
+                 	<div>
+                 		<table class="table table-bordered positionin">
+                 			<tbody>
+                 				<tr class="success">
+							        <td class="h_table" style="background-color: #7C0000">กรรมการ</td>
+							        <td >
+							        	<table>
+								          <tr>
+								            <td>Peter</td>
+								          </tr>
+								          <tr>
+								            <td>Peter</td>
+								          </tr>
+
+								        </table>
+							        </td>
+							      
+						        </tr>
+                 			</tbody>
+                 		</table>
+                 	</div>
+                 	
+                 	<!--g3-->
+                 	<div>
+                 		<table class="table table-bordered positionin">
+                 			<tbody>
+                 				<tr class="success">
+							        <td class="h_table" style="background-color: #7C0000">กรรมการ</td>
+							        <td >
+							        	<table>
+								          <tr>
+								            <td>Peter</td>
+								          </tr>
+								          <tr>
+								            <td>Peter</td>
+								          </tr>
+
+								        </table>
+							        </td>
+							      
+						        </tr>
+                 			</tbody>
+                 		</table>
+                 	</div>
+
+                </div>
+              </div>
+            </form>
+				<!-- End Body -->
+			</div>
+		</div>
+	</div>
+</body>
+<!--#### Footer ####-->
+
+<footer>
+	<div id='ui_footer'></div>
+</footer>
+<!--### End #-->
+</html>
+
+<!-- ==================== Insert Picture ==================== -->
